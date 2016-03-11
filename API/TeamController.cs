20d52f62faeb70dcd0e62f5e32dc8caa2a133831
Taylor Michels <tmichels@dittmanincentives.com>
2016-03-11 11:27:22 -0500

    deleteing comments

--- API/TeamController.cs
+++ API/TeamController.cs
@@ -9,41 +9,11 @@ using TeamSeasonEnders.Models;
 namespace TeamSeasonEnders.API
 {
     public class TeamController : ApiController
-    {
-        // GET api/<controller>
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //// GET api/<controller>/5
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/<controller>
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT api/<controller>/5
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/<controller>/5
-        //public void Delete(int id)
-        //{
-        //}
-        
+    {        
         public IEnumerable<TeamModel> GetTeams(string division)
         {
             using (TeamSeasonEndersEntities context = new TeamSeasonEndersEntities())
             {
-                //var x = context.Teams.Where(t => t.Division == division).ToList();
-                //return x;
-
                 var x = context.Teams.Where(t => t.Conference == division)
                     .Select(r => new TeamModel
                     {
