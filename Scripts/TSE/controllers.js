eb6a0a728a757b8bf29e16da070f5b350f0ba01f
Taylor Michels <tmichels@dittmanincentives.com>
2016-05-18 15:42:45 -0400

    adding filter and images

--- Scripts/TSE/controllers.js
+++ Scripts/TSE/controllers.js
@@ -5,15 +5,18 @@
       scope.division = 'Eastern'; // TODO get from favorite team
       scope.team = '14';
       scope.rival = '';
+      scope.default = { 'Name': 'Select a Rival', 'Id': '0' };
       scope.results = [];
       var init = function () {
           data.getTeams(scope.division).success(function (result) {
               scope.teams = result;
+              scope.teams.unshift(scope.default);
+              scope.rival = scope.teams[0];
           });
       };
 
       scope.update = function () {
-        data.getResults(scope.team, scope.rival).success(function (result) {
+        data.getResults(scope.team, scope.rival.Id).success(function (result) {
             scope.results = result;                  
         });                    
       };
---
+++ Scripts/TSE/filters.js
@@ -0,0 +1,17 @@
+﻿angular.module('formatters.logo', []).
+ filter('logoFormatter', function () {               
+     return function (teamName) {
+         var result;
+
+         switch (teamName) {
+             case 'Rangers':
+                 result = 'https://upload.wikimedia.org/wikipedia/commons/a/ae/New_York_Rangers.svg';
+                 //result = '/Images/RangersLogo.png';
+                 break;
+             case 'Bruins':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/1/12/Boston_Bruins.svg';
+                 break;
+         }         
+         return result;
+     }
+ });
