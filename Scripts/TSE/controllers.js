4ec15fdd2b9898da5effc0e4ab2769dbdf160c3b
Taylor Michels <tmichels@dittmanincentives.com>
2016-07-22 17:01:41 -0400

    differentiate between Team and Rival controller

--- Scripts/TSE/controllers.js
+++ Scripts/TSE/controllers.js
@@ -1,5 +1,5 @@
 ﻿angular.module('controllers.team', [])
-  .controller('TeamController', ['$scope', 'dataFactory', 'colorService', function (scope, data, color) {
+  .controller('RivalController', ['$scope', 'dataFactory', 'colorService', function (scope, data, color) {
       scope.teams = [];
       //scope.divisions = ['Eastern', 'Western'];
       scope.division = 'Eastern'; // TODO get from favorite team
@@ -12,15 +12,36 @@
               scope.teams = result;
               scope.teams.unshift(scope.default);
               scope.rival = scope.teams[0];
-              color.setBackgroundColor();
+              color.setBackgroundColor(); // remove this
           });
       };
 
       scope.update = function () {
-        data.getResults(scope.team, scope.rival.Id).success(function (result) {
-            scope.results = result;                  
-        });                    
+          data.getResults(scope.team, scope.rival.Id).success(function (result) {
+              scope.results = result;                  
+          });                    
       };
 
       init();
-  }]);
+  }]).controller('TeamController', ['$scope', '$rootScope', 'dataFactory', 'colorService', function (scope, rootScope, data, color) {
+      scope.teams = [];
+      //scope.divisions = ['Eastern', 'Western'];
+      scope.division = 'Eastern'; // TODO get from favorite team
+      scope.team = '14';
+      scope.rival = '';
+      scope.default = { 'Name': 'Select a Rival', 'Id': '0' };
+      scope.results = [];
+      var init = function () {
+          data.getTeams(scope.division).success(function (result) {
+              scope.teams = result;
+              scope.teams.unshift(scope.default);
+              scope.rival = scope.teams[0];
+              color.setBackgroundColor();              
+          });
+      };
+
+      scope.update = function() {
+          rootScope.HomeTeam = scope.team;
+      }
+      init();
+}]);   
