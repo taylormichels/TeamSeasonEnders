3efd5e0de3cf8201d062a4d9701dbc90519ebe50
taylormichels <taylormichels@gmail.com>
2017-08-03 16:42:19 -0400

    adding logos for teams in western conference

--- Scripts/TSE/filters.js
+++ Scripts/TSE/filters.js
@@ -16,9 +16,6 @@
              case 'Canadiens':
                  result = 'https://upload.wikimedia.org/wikipedia/commons/6/69/Montreal_Canadiens.svg';
                  break;
-             case 'Ducks':
-                 result = 'https://upload.wikimedia.org/wikipedia/en/7/72/Anaheim_Ducks.svg';
-                 break;
              case 'Sabres':
                  result = 'https://upload.wikimedia.org/wikipedia/en/6/6f/Buffalo_Sabres.svg';
                  break;
@@ -54,6 +51,49 @@
                  break;
              case 'Capitals':
                  result = 'https://upload.wikimedia.org/wikipedia/en/2/2d/Washington_Capitals.svg';
+                 break;
+              // begin Western -----------------------------------------------------------------------
+             case 'Ducks':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/7/72/Anaheim_Ducks.svg';
+                 break;
+             case 'Coyotes':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/2/27/Arizona_Coyotes.svg';
+                 break;
+             case 'Flames': 
+                 result = 'https://upload.wikimedia.org/wikipedia/en/6/60/Calgary_Flames_Logo.svg';
+                 break;
+             case 'Oilers':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/4/4d/Logo_Edmonton_Oilers.svg';
+                 break;
+             case 'Kings':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/c/cb/Los_Angeles_Kings_Logo_%282011%29.svg';
+                 break;
+             case 'Sharks':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/3/37/SanJoseSharksLogo.svg';
+                 break;
+             case 'Canucks':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/3/3a/Vancouver_Canucks_logo.svg';
+                 break;
+             case 'Blackhawks':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/2/29/Chicago_Blackhawks_logo.svg';
+                 break;
+             case 'Avalanche':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/4/45/Colorado_Avalanche_logo.svg';
+                 break;
+             case 'Stars':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/c/ce/Dallas_Stars_logo_%282013%29.svg';
+                 break;
+             case 'Wild':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/1/1b/Minnesota_Wild.svg';
+                 break;
+             case 'Predators':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/9/9c/Nashville_Predators_Logo_%282011%29.svg';
+                 break;
+             case 'Blues':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/e/ed/St._Louis_Blues_logo.svg';
+                 break;
+             case 'Jets':
+                 result = 'https://upload.wikimedia.org/wikipedia/en/9/93/Winnipeg_Jets_Logo_2011.svg';
                  break;
              default:
                  result = 'https://upload.wikimedia.org/wikipedia/commons/3/33/Image-missing.svg';
